e1edcbb9bd2d1cd12b8c3a135506326f274bdfaa
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2023-03-05 23:17:03 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-acount_sid = "AC4a56e9ad930d55e736d198401baa08d8"
-auth_token = "bd828e3d0cbc821ee2dcb57f74ae176b"
-twilio_number = "+18129432525"
-my_number = "+917829808808"
+acount_sid = "your account sid"
+auth_token = "your auth token "
+twilio_number = "twilio number"
+my_number = "registered number"
